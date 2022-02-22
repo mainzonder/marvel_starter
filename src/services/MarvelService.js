82df1627caea9c109268d16420dad6de65588a3b
mainzonder <48353743+mainzonder@users.noreply.github.com>
2022-02-22 22:53:53 +0200

    001

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
     _apiBase = ' https://gateway.marvel.com:443/v1/public/'
     _apiKey = 'apikey=a410f1b9138f1bdd94e25f617fbd6a79'
+    _baseOffset = 210;
 
 
     getResource = async (url) => {
@@ -14,8 +15,8 @@ class MarvelService {
 
 
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`)
         return res.data.results.map(this._transformCharacter);
     }
 
